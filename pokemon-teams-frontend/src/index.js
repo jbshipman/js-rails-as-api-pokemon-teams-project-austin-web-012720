8cf7331d45f1cb7539435603c43f31e9fe5d9600
jbshipman <jbshipman@icloud.com>
2020-03-14 07:42:29 -0500

    wip-001

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,39 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+
+document.addEventListener("DOMContentLoaded", () => {
+  fetchTrainers();
+  // fetchPokemons();
+});
+
+function fetchTrainers () {
+  fetch('http://localhost:3000/trainers')
+    .then(resp => resp.json())
+    .then(tobj => {
+      console.log(tobj);
+      renderTrainerCards(tobj);
+    });
+};
+
+// function fetchPokemons (trainer, stored) {
+//   fetch('http://localhost:3000/pokemons')
+//     .then(resp => resp.json())
+//     .then(obj => console.log(obj));
+// };
+let pokemonList = [];
+// forEach trainer to build card
+// forEach pokemon per trainer to put into card that was built
+function renderTrainerCards(trainers) {
+  const main = document.getElementById('container');
+  const ul = document.createElement('ul');
+
+  trainers.forEach(trainer => {
+    main.innerHTML += `
+    <div class="card" data-id=${trainer.id}><p>${trainer.name}</p>
+    <button data-trainer-id=${trainer.id}>Add Pokemon</button>
+    
+    </div>`
+  });
+
+};
