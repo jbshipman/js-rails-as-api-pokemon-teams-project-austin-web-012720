f89395119e155b340d6ef5a299fc83457c04e40e
jbshipman <jbshipman@icloud.com>
2020-03-15 10:08:16 -0500

    make display work

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -2,38 +2,47 @@ const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
 
+console.log("Welcome to the console log");
+//  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// my wip
+// user story 1
+
+
+function fetchTrainers () {
+  fetch(TRAINERS_URL)
+  .then(resp => resp.json())
+  .then(jdata => {
+    // console.log(jdata);
+    renderTrainerCards(jdata);
+  });
+};
+
+function renderTrainerCards(jdata) {
+  const trainerCards = document.getElementById('container');
+
+  //build out the trainer card
+  jdata.forEach(trainer => {      
+    let pokeUl = document.createElement('ul');
+    trainer.pokemons.forEach(pokemon => {     // inner loop to build out pokemon list
+      pokeUl.innerHTML += `
+      <li>${pokemon.nickname} (${pokemon.species}) <button class="release" data-pokemon-id=${pokemon.id}>Release</button></li>`;
+    });
+    // outer loop for trainer card
+    trainerCards.innerHTML += `
+      <div class="card" data-id=${trainer.id}><p>${trainer.name}</p>
+        <button data-trainer-id=${trainer.id}>Add Pokemon</button>
+        <ul id=${trainer.id}>
+          ${pokeUl.innerHTML}
+        </ul>
+      </div>`;
+    // console.log(pokeUl)
+  });
+
+
+};
+
+
 document.addEventListener("DOMContentLoaded", () => {
   fetchTrainers();
   // fetchPokemons();
 });
-
-function fetchTrainers () {
-  fetch('http://localhost:3000/trainers')
-    .then(resp => resp.json())
-    .then(tobj => {
-      console.log(tobj);
-      renderTrainerCards(tobj);
-    });
-};
-
-// function fetchPokemons (trainer, stored) {
-//   fetch('http://localhost:3000/pokemons')
-//     .then(resp => resp.json())
-//     .then(obj => console.log(obj));
-// };
-let pokemonList = [];
-// forEach trainer to build card
-// forEach pokemon per trainer to put into card that was built
-function renderTrainerCards(trainers) {
-  const main = document.getElementById('container');
-  const ul = document.createElement('ul');
-
-  trainers.forEach(trainer => {
-    main.innerHTML += `
-    <div class="card" data-id=${trainer.id}><p>${trainer.name}</p>
-    <button data-trainer-id=${trainer.id}>Add Pokemon</button>
-    
-    </div>`
-  });
-
-};
